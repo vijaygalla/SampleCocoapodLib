c5a5bef38e0b60a41c7801a601f830b45beab266
Vijay Bhaskar <kemisettykalyani308@gmail.com>
2023-11-13 12:12:30 +0530

    Test cases added

--- Sources/SwiftCocoaPodLib/SwiftCocoaPodLib.swift
+++ Sources/SwiftCocoaPodLib/SwiftCocoaPodLib.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public final class SwiftCocoaPodLib {
+    
+    let name = "SwiftCocoaPodLib"
+    
+    public func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+}
--- Tests/SwiftCocoaPodLibTests/SwiftCocoaPodLibTests.swift
+++ Tests/SwiftCocoaPodLibTests/SwiftCocoaPodLibTests.swift
@@ -8,5 +8,9 @@ final class SwiftCocoaPodLibTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        
+        let obj = SwiftCocoaPodLib()
+        XCTAssertEqual(obj.add(2, 1), 3)
+        XCTAssertEqual(obj.sub(2, 1), 1)
     }
 }
